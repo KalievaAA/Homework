b75d1b26865e1ca25892a0c07e48e5e2bf1676f2
KalievaAA <malhosyan89@gmail.com>
2024-11-10 18:46:34 +0500

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,7 @@
-Kolichestvo_vypolnennykh_DZ = 12
-Kolichestvo_zatrachennykh_chasov = 1.5
-Nazvanie_kursa = ("Python")
-Vremya_na_odno_zadanie = Kolichestvo_zatrachennykh_chasov / Kolichestvo_vypolnennykh_DZ
-print("Курс:", Nazvanie_kursa, ",",
-      "всего задач:", Kolichestvo_vypolnennykh_DZ,",",
-      "затрачено часов:", Kolichestvo_zatrachennykh_chasov,",",
-      "среднее время выполнения:", Vremya_na_odno_zadanie, "часа" + ".")
+my_string = input('How are you today? ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
