fe02d8c5da39313dc178392f8d8e931194edfb2d
KalievaAA <malhosyan89@gmail.com>
2024-11-10 18:47:08 +0500

    Add files via upload

---
+++ pw4_(Peremennye).py
@@ -0,0 +1,8 @@
+Kolichestvo_vypolnennykh_DZ = 12
+Kolichestvo_zatrachennykh_chasov = 1.5
+Nazvanie_kursa = ("Python")
+Vremya_na_odno_zadanie = Kolichestvo_zatrachennykh_chasov / Kolichestvo_vypolnennykh_DZ
+print("Курс:", Nazvanie_kursa, ",",
+      "всего задач:", Kolichestvo_vypolnennykh_DZ,",",
+      "затрачено часов:", Kolichestvo_zatrachennykh_chasov,",",
+      "среднее время выполнения:", Vremya_na_odno_zadanie, "часа" + ".")
